35e90cb0dfe6b09fb2df0d2f399231fe3a522f73
DK <dakorsun@gmail.com>
2024-05-18 21:20:06 +0300

    feat: github login

--- src/env.ts
+++ src/env.ts
@@ -5,10 +5,15 @@ export const env = createEnv({
   server: {
     GITHUB_CLIENT_ID: z.string(),
     GITHUB_SECRET: z.string(),
+    NEXTAUTH_SECRET: z.string(),
   },
-  client: {},
+  client: {
+    NEXT_PUBLIC_NEXTAUTH_URL: z.string(),
+  },
   runtimeEnv: {
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_SECRET: process.env.GITHUB_SECRET,
+    NEXT_PUBLIC_NEXTAUTH_URL: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
 });
